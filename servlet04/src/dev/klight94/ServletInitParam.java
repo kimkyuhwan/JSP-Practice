b5c758d66c2b735f2f66da66c276bbb7e9de807a
김규환 <gmarble94@gmail.com>
2019-08-06 19:42:09 +0900

    servlet init parameters

---
+++ servlet04/src/dev/klight94/ServletInitParam.java
@@ -0,0 +1,63 @@
+package dev.klight94;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * Servlet implementation class ServletInitParam
+ */
+@WebServlet(urlPatterns= {"/ServletInitParam"}, initParams={@WebInitParam(name="id", value="aaaaa"), @WebInitParam(name="pw", value="99999")})
+public class ServletInitParam extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletInitParam() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		request.setCharacterEncoding("EUC-KR");
+		
+		String id = getInitParameter("id");
+		String pw = getInitParameter("pw");
+		String path = getInitParameter("path");
+		
+		response.setContentType("text/html; charset=EUC-KR");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html> <head> </head> <body>");
+		writer.println("아이디 : "+id+"<br />");
+		writer.println("비밀번호 : "+pw+"<br />");
+		writer.println("경로 : "+path+"<br />");
+		writer.println("</body> </html>");
+		
+		writer.close();
+		
+		
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
